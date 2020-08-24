25f8e34f98b255ba78f64c86f3a14b06d646d08b
Nick <nickberezny@hotmail.com>
2020-08-24 18:31:16 -0400

    added simple unit test example

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public float multiply2Numbers(float a, float b)
+    {
+        return a * b;
+    }
+}
---
+++ Assets/Tests/EditorMode/ExampleTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ExampleTest
+    {
+        private GameObject _root;
+        
+        
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _root = new GameObject();
+            _root.AddComponent<ExampleScript>();
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ExampleTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual(6f, _root.GetComponent<ExampleScript>().multiply2Numbers(3f, 2f));
+        }
+
+    }
+}
